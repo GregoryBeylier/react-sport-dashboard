c19e4b1357e850fbc5daa167b0e3eb321a121b10
Gregory <samsung261101@gmail.com>
2026-06-03 16:08:30 +0200

    feat: implement application footer
    Introduces a fully styled footer component using CSS Modules, complete with copyright information, navigation links, and the SportSee logo.
    Also includes a global reset of body margin and padding to ensure consistent layout.

--- sportsee/app/components/layout/footer/Footer.tsx
+++ sportsee/app/components/layout/footer/Footer.tsx
@@ -1,10 +1,15 @@
+import styles from "./Footer.module.css";
+import incoLogo from "../../../public/images/Iconlogo.png";
 
 export default function Footer() {
-    return (
-        <div>
-            <div>©Sportsee Tous droits réservés</div>
-            <div>Conditions générales Contact</div>
-            <div>Logo</div>
-        </div>
-    );
+  return (
+    <div className={styles.footer}>
+      <div>©Sportsee Tous droits réservés</div>
+      <div className={styles.right}>
+        <span>Conditions générales</span>
+        <span>Contact</span>
+        <img src={incoLogo} alt="SportSee logo" />
+      </div>
+    </div>
+  );
 }
